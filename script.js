8f3e527f9ed3fef29d64bae8b2738371ba3be42c
rika20007 <rikablak75@gmail.com>
2026-07-15 15:03:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,46 +2,30 @@ emailjs.init({
     publicKey: "jzLFk-5zLU3DAHhj3"
 });
 
-
 const form = document.getElementById("feelingForm");
 const msg = document.getElementById("msg");
 
-
 form.addEventListener("submit", function(e) {
-
     e.preventDefault();
-
 
     const data = {
         user_email: document.getElementById("email").value,
         truth: document.getElementById("truth").value
     };
 
-
     emailjs.send(
         "service_v67j4dg",
         "template_fzn126i",
         data
     )
-
     .then(function() {
-
         msg.style.color = "green";
         msg.innerHTML = "تم إرسال الحقيقة بنجاح ✔";
-
         form.reset();
-
     })
-
     .catch(function(error) {
-
-    msg.style.color = "red";
-    msg.innerHTML = "حدث خطأ أثناء الإرسال";
-
-    console.log(error);
-
-});
-
+        msg.style.color = "red";
+        msg.innerHTML = "حدث خطأ أثناء الإرسال";
+        console.log(error);
     });
-
 });
